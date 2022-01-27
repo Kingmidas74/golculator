7ee46743345ef754f23f6f79be4558f5a4d24ab7
Kingmidas74 <kingmidas1992@gmail.com>
2022-01-27 07:50:43 +0300

    extend IArrayProvider

--- core/helpers/arrays.go
+++ core/helpers/arrays.go
@@ -2,19 +2,34 @@ package helpers
 
 type IArrayProvider interface {
 	ReverseFloatArray(input []float64) []float64
+	ReverseStringArray(input []string) []string
+	ReverseComplexArray(input []complex128) []complex128
 }
 
 type ArrayProvider struct {
-
 }
 
 func NewArrayProvider() IArrayProvider {
 	return &ArrayProvider{}
 }
 
-func(this *ArrayProvider) ReverseFloatArray(input []float64) []float64 {
+func (this *ArrayProvider) ReverseFloatArray(input []float64) []float64 {
 	if len(input) == 0 {
 		return input
 	}
 	return append(this.ReverseFloatArray(input[1:]), input[0])
+}
+
+func (this *ArrayProvider) ReverseStringArray(input []string) []string {
+	if len(input) == 0 {
+		return input
+	}
+	return append(this.ReverseStringArray(input[1:]), input[0])
+}
+
+func (this *ArrayProvider) ReverseComplexArray(input []complex128) []complex128 {
+	if len(input) == 0 {
+		return input
+	}
+	return append(this.ReverseComplexArray(input[1:]), input[0])
 }
